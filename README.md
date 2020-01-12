0e07e162bdef0e128cc64b106ddd2051f7b2705a
nfaltir <nfaltir@protonmail.com>
2020-01-11 18:51:51 -0800

    new files added

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 Hello World
 
+<<<<<<< HEAD
 Practice problem
 
 link: https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
+=======
+link: https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
+>>>>>>> Added image of output, fixed user input logic
--- guessNum.py
+++ guessNum.py
@@ -1,4 +1,5 @@
 import random
+<<<<<<< HEAD
 anyNum = random.randint(1,9)
 #print(anyNum) <-------- test
 
@@ -22,11 +23,39 @@ while guessEntry != anyNum and guessEntry != 'Quit':
         
         elif guessEntry < anyNum:
             print ("Too low")
+=======
+anyNum = random.randint(1, 9)
+# print(anyNum) <-------- test
+print("\n=============================High or Low Game=============================")
+guessEntry = 0
+count = 0
+maxRange = int(input("Enter max range:"))
+while guessEntry != anyNum and guessEntry != 'Quit':
+
+    guessEntry = input("Enter a number between 1 "+"and "+str(maxRange)+":")
+
+    if guessEntry == 'Quit':
+        break
+    # sets counter
+    count += 1
+
+    guessEntry = int(guessEntry)
+
+    if guessEntry >= 1 and guessEntry <= 9:
+        if guessEntry > anyNum:
+            print("Too High")
+
+        elif guessEntry < anyNum:
+            print("Too low")
+>>>>>>> Added image of output, fixed user input logic
         elif guessEntry == anyNum:
             print("\nBINGO YOU DID IT, the answer is:", anyNum)
             print("\nit took you", count, "tries\n")
     else:
         invalidEntry = guessEntry
         print("invalid entry, please use a number between 1-9")
+<<<<<<< HEAD
 
     
+=======
+>>>>>>> Added image of output, fixed user input logic
