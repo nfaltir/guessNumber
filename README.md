2bd8bdc28bb8bb832f386840860098244eb5be6b
nfaltir <nfaltir@protonmail.com>
2020-01-11 19:11:51 -0800

    Fixed range logic, added output image

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-Hello World
+Program Name: Guess High or Low
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 Practice problem
 
@@ -7,3 +8,18 @@ link: https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.ht
 =======
 link: https://www.practicepython.org/exercise/2014/04/02/09-guessing-game-one.html
 >>>>>>> Added image of output, fixed user input logic
+=======
+
+
+Input
+1. enter a number (sets range)
+
+Output
+1. Asks user to guess the right number between 1 and the user input (int)
+2. program will generate a random answer
+3. if the guess > answer = Too high
+4. if the guess < answer = Too low
+5. if the guess = answer = "Bingo You Won!"
+6. Program will generate total number of tries
+7. program ends!
+>>>>>>> fixed range logic, added image of output
--- guessNum.py
+++ guessNum.py
@@ -1,35 +1,11 @@
 import random
-<<<<<<< HEAD
-anyNum = random.randint(1,9)
-#print(anyNum) <-------- test
-
-guessEntry = 0
-count =0
-
-while guessEntry != anyNum and guessEntry != 'Quit':
-
-    guessEntry = input("Enter a number between 1 and 9:")
-
-    if guessEntry == 'Quit':
-        break
-    #sets counter
-    count +=1
-    
-    guessEntry = int(guessEntry)
-
-    if guessEntry >=1 and guessEntry <= 9:
-        if guessEntry > anyNum:
-            print("Too High")
-        
-        elif guessEntry < anyNum:
-            print ("Too low")
-=======
-anyNum = random.randint(1, 9)
+#anyNum = random.randint(1, 9)
 # print(anyNum) <-------- test
 print("\n=============================High or Low Game=============================")
 guessEntry = 0
 count = 0
 maxRange = int(input("Enter max range:"))
+anyNum = random.randint(1, maxRange)
 while guessEntry != anyNum and guessEntry != 'Quit':
 
     guessEntry = input("Enter a number between 1 "+"and "+str(maxRange)+":")
@@ -41,21 +17,15 @@ while guessEntry != anyNum and guessEntry != 'Quit':
 
     guessEntry = int(guessEntry)
 
-    if guessEntry >= 1 and guessEntry <= 9:
+    if guessEntry >= 1 and guessEntry <= maxRange:
         if guessEntry > anyNum:
             print("Too High")
 
         elif guessEntry < anyNum:
             print("Too low")
->>>>>>> Added image of output, fixed user input logic
         elif guessEntry == anyNum:
             print("\nBINGO YOU DID IT, the answer is:", anyNum)
             print("\nit took you", count, "tries\n")
     else:
         invalidEntry = guessEntry
-        print("invalid entry, please use a number between 1-9")
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> Added image of output, fixed user input logic
+        print("\ninvalid entry, please use a number between 1 and "+str(maxRange),"\n")
